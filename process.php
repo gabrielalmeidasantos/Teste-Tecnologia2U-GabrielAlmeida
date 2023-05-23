5b1fa94a76721080fd12f15c0ed2aef5627ce02f
gabrielalmeidasantos <gabriel.almeiidda@gmail.com>
2023-05-22 21:42:02 -0300

    adicionando arquivo que fara o processamento da requisicao enviada pelo formulario

---
+++ process.php
@@ -0,0 +1,22 @@
+<?php
+
+require("./config.php");
+require("./dao/UsuarioDAO.php");
+
+if ($_POST) {
+    $nome = $_POST['nome'];
+    $cep = $_POST['cep'];
+    $logradouro = $_POST['logradouro'];
+    $cidade = $_POST['cidade'];
+    $bairro = $_POST['bairro'];
+    $estado = $_POST['estado'];
+
+    if (isset($nome) && isset($nome) && isset($logradouro) && isset($cidade) && isset($bairro) && isset($estado)) {
+        $usuarioDAO = new usuarioDAO($conn);
+        $usuario = new Usuario($nome, $cep, $logradouro, $cidade, $bairro, $estado);
+        $retorno = $usuarioDAO->registrar($usuario);
+        $_SESSION['retorno'] = ["mensagem" => $retorno["mensagem"], "erro" => $retorno["erro"]];
+    }
+}
+
+header("location: ./index.php");
